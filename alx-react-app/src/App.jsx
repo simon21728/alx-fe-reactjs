0bca93331ec43db43bb15efa93b6f9f150a6b614
Simon <simonbayu2211@exgmail.com>
2026-01-09 10:13:58 +0300

    Create a User Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,15 +1,25 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <>
-      <Header />
+    <Header />
       <MainContent />
       <Footer />
+      
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+      
     </>
   );
 }
 
 export default App;
+
+
