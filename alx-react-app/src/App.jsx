c8729c7ceaffffdb94bc89682509a4499ebed2f2
Simon <simonbayu2211@exgmail.com>
2026-01-09 10:19:17 +0300

    Modify JSX in a Pre-built React Component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,20 +2,22 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
   return (
     <>
-    <Header />
-      <MainContent />
-      <Footer />
-      
+    <WelcomeMessage />
+    
+
       <UserProfile
         name="Alice"
         age="25"
         bio="Loves hiking and photography"
       />
-      
+    <Header />
+    <MainContent />
+    <Footer />
     </>
   );
 }
