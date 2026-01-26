34a44bdb12260ae1e36cd5a2e7e6fab48d4ec4d6
Simon <simonbayu2211@exgmail.com>
2026-01-26 12:57:00 +0300

    remane folder

---
+++ github_user_search/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <Header />
+
+      
+      <Search />
+
+      <p>Project setup completed successfully </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ github_user_search/src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>GitHub User Search</h1>;
+}
+
+export default Header;
